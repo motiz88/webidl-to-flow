e8e9d5521b812c6c5b5d9aa367f78c11d809abfd
motiz88 <motiz88@gmail.com>
2015-05-31 01:23:16 +0300

    Untrack flow-experiment

--- flow-experiment/interface/A.js
+++
@@ -1,4 +0,0 @@
-interface I {}
-type T = {};
-new I();	// <--- No error!
-new T();	// Identifier T: could not resolve name
--- flow-experiment/interface/named-constructor.js
+++
@@ -1,4 +0,0 @@
-class HTMLAudioElement {}
-declare function Audio(src?: string): HTMLAudioElement;
-var a: HTMLAudioElement = new Audio();
-
